ca37c576adcd4c87df4e9419570ef6dc53ed514d
BeardedSAM <agudarustefan@gmail.com>
2024-04-30 17:25:28 +0300

    feat: add-lambada

--- terraform-mkdocs-module/main.tf
+++ terraform-mkdocs-module/main.tf
@@ -61,7 +61,7 @@ resource "aws_iam_role" "iam_for_lambda" {
 }
 
 resource "aws_lambda_function" "append_index_html" {
-  s3_bucket     = "arn:aws:s3:::mkdocs-bucket" 
+  filename         = "lambda.js"
   function_name    = "AppendIndexHtmlLambda"
   role             = aws_iam_role.iam_for_lambda.arn
 }
