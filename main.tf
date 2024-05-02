86fa92426a37636b14adc996d894e2b2595bcd70
BeardedSAM <agudarustefan@gmail.com>
2024-05-02 17:56:28 +0300

    change-module-name

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
-module "terraform-s3-cloudfront-lambda" {
+module "terraform_s3_cloudfront_lambda" {
   source                = "./terraform-mkdocs-module"
   bucket_name           = var.bucket_name
   cloudfront_price_class = var.cloudfront_price_class
 }
 
 output "bucket_endpoint" {
-  value = module.mkdocs.bucket_endpoint
+  value = module.terraform_s3_cloudfront_lambda.bucket_endpoint
 }
 
 output "cloudfront_domain_name" {
-  value = module.mkdocs.cloudfront_domain_name
+  value = module.terraform_s3_cloudfront_lambda.cloudfront_domain_name
 }
