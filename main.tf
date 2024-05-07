47b9086e3e03c22e97ce5a78960ba6da6494b930
AgudaruStefan <97911661+AgudaruStefan@users.noreply.github.com>
2024-05-07 11:30:00 +0300

    change-module-name (#15)
    * change-module-name
    
    * change-module-name
    
    * change-module-name
    
    * add_moved

--- main.tf
+++ main.tf
@@ -1,13 +1,18 @@
-module "mkdocs" {
+module "terraform_s3_cloudfront_lambda_func" {
   source                = "./terraform-mkdocs-module"
   bucket_name           = var.bucket_name
   cloudfront_price_class = var.cloudfront_price_class
 }
 
 output "bucket_endpoint" {
-  value = module.mkdocs.bucket_endpoint
+  value = module.terraform_s3_cloudfront_lambda_func.bucket_endpoint
 }
 
 output "cloudfront_domain_name" {
-  value = module.mkdocs.cloudfront_domain_name
+  value = module.terraform_s3_cloudfront_lambda_func.cloudfront_domain_name
+}
+
+moved {
+  from = module.mkdocs
+  to = module.terraform_s3_cloudfront_lambda_func
 }
