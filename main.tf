e35010fdc3c804600bd59f6791816274080435c4
BeardedSAM <agudarustefan@gmail.com>
2024-05-07 11:11:13 +0300

    add_moved

--- main.tf
+++ main.tf
@@ -11,3 +11,8 @@ output "bucket_endpoint" {
 output "cloudfront_domain_name" {
   value = module.terraform_s3_cloudfront_lambda_func.cloudfront_domain_name
 }
+
+moved {
+  from = module.mkdocs
+  to = module.terraform_s3_cloudfront_lambda_func
+}
