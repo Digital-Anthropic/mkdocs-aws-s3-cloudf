85020cda509429f53b7d9899acc15a35b1f13e4e
BeardedSAM <agudarustefan@gmail.com>
2024-05-08 19:20:04 +0300

    feat: add-bucket-for-modules

--- locals.tf
+++ locals.tf
@@ -1,7 +1,13 @@
 locals {
     terraform_s3_cloudfront_lambda_func = {
+      
     "mkdocs-bucket" = {
       bucket_name               = "mkdocs-bucket"
+
+    }
+
+    "bucket-for-modules" = {
+      bucket_name               = "bucket-for-modules"
 
     }
 }
