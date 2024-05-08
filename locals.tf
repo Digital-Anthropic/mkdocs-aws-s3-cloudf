e666d29ee9c0b084ddb3fb06ad7afdf9bffce61c
AgudaruStefan <97911661+AgudaruStefan@users.noreply.github.com>
2024-05-08 18:56:12 +0300

    feat: add-locals (#17)
    * feat: add-locals
    
    * feat: add-locals
    
    * feat: add-locals
    
    * feat: add-locals
    
    * feat: add-locals
    
    * feat: add-locals
    
    * feat: add-locals

---
+++ locals.tf
@@ -0,0 +1,13 @@
+locals {
+    terraform_s3_cloudfront_lambda_func = {
+    "mkdocs-bucket" = {
+      bucket_name               = "mkdocs-bucket"
+
+    }
+    
+    "modules-bucket" = {
+      bucket_name               = "modules-bucket"
+
+    }
+}
+}
--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 module "terraform_s3_cloudfront_lambda_func" {
+  for_each = local.terraform_s3_cloudfront_lambda_func
   source                = "./s3-cloudfront-module"
-  bucket_name           = var.bucket_name 
+  bucket_name           = each.value.bucket_name
   cloudfront_price_class = var.cloudfront_price_class
   lambda-arn = {
     "lambda-arn" = {
@@ -9,19 +10,24 @@ module "terraform_s3_cloudfront_lambda_func" {
   }
 }
 
+
 module "lambda_edge" {
   source                = "./lambda-module"
 }
 
-output "bucket_endpoint" {
-  value = module.terraform_s3_cloudfront_lambda_func.bucket_endpoint
+output "bucket_endpoints" {
+  value = {
+    for key, value in module.terraform_s3_cloudfront_lambda_func : key => value.bucket_endpoint
+  }
 }
 
-output "cloudfront_domain_name" {
-  value = module.terraform_s3_cloudfront_lambda_func.cloudfront_domain_name
+output "cloudfront_domain_names" {
+  value = {
+    for key, value in module.terraform_s3_cloudfront_lambda_func : key => value.cloudfront_domain_name
+  }
 }
 
 moved {
-  from = module.mkdocs
-  to = module.terraform_s3_cloudfront_lambda_func
+  from = module.terraform_s3_cloudfront_lambda_func
+  to = module.terraform_s3_cloudfront_lambda_func["mkdocs-bucket"]
 }
